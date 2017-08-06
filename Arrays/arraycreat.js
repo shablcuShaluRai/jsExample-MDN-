9412bff203bd21a23c600efd9394705e560e39c1
shalurai <shaluraics22@gmail.com>
2017-08-06 22:00:40 +0900

    create  an array

---
+++ Arrays/arraycreat.js
@@ -0,0 +1,15 @@
+//1. creating an array using array literal
+
+// JavaScript arrays are used to store multiple values in a single variable.
+// An array is a special variable, which can hold more than one value at a time.
+  var breakfast = ["coffee","tea","bread","chapati"];
+  console.log(breakfast);
+
+// javascript also contains different type of value in the same variable.
+ var hodgePodge = [100,"paint",[200, "brush"],false];
+ console.log(hodgePodge);
+
+
+// 2. creating an array using new keyword.
+ var cars = new Array("Sab","volvo","BMW");
+  console.log(cars);
