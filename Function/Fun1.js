dad8c9cdd131364865222641ebc4fb7d4b637d40
shalurai <shaluraics22@gmail.com>
2017-08-06 11:30:33 +0900

    function example

---
+++ Function/Fun1.js
@@ -0,0 +1,20 @@
+
+// a function is a block of code designed to perform a task.
+// it takes a number of arguments.
+
+var multiplication = function(a,b){
+  return a*b;
+}
+
+console.log(multiplication(4,5));
+
+
+
+// second method for multiplication
+
+
+var multiplication = function(a,b){
+   console.log(a*b);
+}
+
+multiplication(6,5);
---
+++ Object/ObjDef.js
@@ -0,0 +1,15 @@
+
+
+//  An object is a collection of properties, and a property is an association between a name (or key) and a value.
+// A property can be a function , in which case the property is known as method.
+
+//An object can be created by using object literal
+
+ var person = {
+   firstName:"Shalu",
+   Lastname: "Rai",
+   age:25,
+
+ }
+
+console.log(person.name + " "+ person.age);
