42f742e7f3e5ba1fd43a95db36e0eb1b0dc6fa1a
shalurai <shaluraics22@gmail.com>
2017-08-07 10:09:48 +0900

    length of the array

---
+++ Arrays/arraylen.js
@@ -0,0 +1,10 @@
+
+// find the length of array
+
+var  fruits = ["apple","mango","peers","litchi","banana"];
+ console.log(fruits.length);
+ for(var i=0;i<fruits.length;i++){
+    fruit = fruits[i];
+    // print the element from the array.
+     console.log(fruit);
+ }
