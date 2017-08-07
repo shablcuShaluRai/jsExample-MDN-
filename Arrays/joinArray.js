7f8e41f49d7d7c6518cce06e9d637d0cfac355a8
shalurai <shaluraics22@gmail.com>
2017-08-08 00:24:56 +0900

    join the elements of array

---
+++ Arrays/joinArray.js
@@ -0,0 +1,6 @@
+// join() joins all elements of the array into string.
+
+var weather = ['summer','winter','rain','air'];
+console.log(weather.join()); // summer,winter,rain,air(default seperator comma)
+console.log(weather.join('-'));// we get the result using separator.//summer-winter-rain-air
+console.log(weather.join(" ")); //summer winter rain air
