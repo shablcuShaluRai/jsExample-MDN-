1d45af688056e48db72401b5eb788586b5f69494
shalurai <shaluraics22@gmail.com>
2017-08-08 00:16:07 +0900

    return index of the element

--- Arrays/indexOfarr.js
+++ Arrays/indexOfarr.js
@@ -1,12 +1,13 @@
 // //The indexOf() method returns the first index at which a given element can be found in the array,
 // // or -1 if it is not present.
 //
-// var arr = [2,5,5,7,10];
-// console.log(arr.indexOf(7)); // return 3,index of element
-// console.log(arr.indexOf(5,2)); // returns 2 , when element found in an array.
-// console.log(arr.indexOf(9)); // return -1 , when element not found in an array
-// console.log(arr.indexOf(10)); //return 4 ,index of element in an array.
-//
+ var arr = [2,5,5,7,10];
+ console.log(arr.indexOf(5)); // return 1,index of the first element
+console.log(arr.indexOf(7)); // return 3,index of element
+console.log(arr.indexOf(5,2)); // returns 2 , when element found in an array.
+console.log(arr.indexOf(9)); // return -1 , when element not found in an array
+console.log(arr.indexOf(10)); //return 4 ,index of element in an array.
+
 // // if matched element found then it print yes otherwise no
 // var arr = [2,5,7,9,3];
 // var k=5;
@@ -24,6 +25,6 @@ while(idx!=-1){
 var pushind = indices.push(idx);
 //  console.log(pushind);
   idx = array.indexOf(element,idx+1);
-//  console.log(idx); 
+//  console.log(idx);
 }
 console.log(indices);
