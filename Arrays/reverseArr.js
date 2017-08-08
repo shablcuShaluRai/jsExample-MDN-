380aa545bbd547949a36ffc1e2bb4bbefc8549be
shalurai <shaluraics22@gmail.com>
2017-08-08 16:58:39 +0900

    reverse the element of array

---
+++ Arrays/reverseArr.js
@@ -0,0 +1,7 @@
+
+// The reverse() method reverse an array in place. The first array element becomes the last ,and the last element array
+// becomes the first.
+
+var sequence = ['one','two','three'];
+console.log(sequence.reverse());//returns [ 'three', 'two', 'one' ]
+console.log(sequence);
