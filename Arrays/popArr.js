2024287dec5d8a9b5f6afb64425431c943e879f2
shalurai <shaluraics22@gmail.com>
2017-08-08 13:03:19 +0900

    pop element from the array

---
+++ Arrays/popArr.js
@@ -0,0 +1,6 @@
+//The pop() method removes the last element from an array and returns that element.
+// This method changes the length of the array.
+
+var arr = [1,2,3];
+console.log(arr.pop()); // returns 3
+console.log(arr); //returns [1,2]
