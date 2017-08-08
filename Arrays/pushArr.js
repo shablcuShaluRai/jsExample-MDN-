ddaf8b9cc6a5fccf69896cca15d8a830b61e0cfd
shalurai <shaluraics22@gmail.com>
2017-08-08 13:30:32 +0900

    merging two arrays using push()

--- Arrays/pushArr.js
+++ Arrays/pushArr.js
@@ -6,3 +6,18 @@ var n=num.push(5,6,7,8,2)
 console.log(num); // returns [1,2,3,4,5,6,7,8]
 // n contains the total length of the array
 console.log(n); // 9
+
+
+// merging two array using push method
+
+var fruits = ['apple','mango','orange','banana'];
+var vegetables = ["potato","tomato"];
+Array.prototype.push.apply(fruits,vegetables);
+console.log(fruits); // return [ 'apple', 'mango', 'orange', 'banana', 'potato', 'tomato' ];
+
+console.log(vegetables); // [ 'potato', 'tomato' ]
+
+
+var num =[1,2,3,4];
+Array.prototype.push.apply(num,[5,6,7,8,9]);
+console.log(num);
