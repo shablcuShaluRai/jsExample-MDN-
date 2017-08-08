d4853a49d20202da4371afe403bdd3fea74f30cc
shalurai <shaluraics22@gmail.com>
2017-08-08 13:06:02 +0900

    pop an empty array

--- Arrays/popArr.js
+++ Arrays/popArr.js
@@ -4,3 +4,7 @@
 var arr = [1,2,3];
 console.log(arr.pop()); // returns 3
 console.log(arr); //returns [1,2]
+
+var array = [];
+//if we call pop() on an empty array then it returns undefined
+console.log(array.pop()); // op- undefined
