b6e8ce52acae71e4d3d055a8401d27ed1f4d88a6
shalurai <shaluraics22@gmail.com>
2017-08-09 12:24:41 +0900

    getting  value and index using for...of loop

--- Arrays/entriesArr.js
+++ Arrays/entriesArr.js
@@ -1,8 +1,19 @@
 //The entries() method returns a new Array Iterator object that contains the key/value pairs for each index in the array.
 
 var a = ['a','b','c'];
-console.log( typeof a.entries())
+console.log( typeof a.entries()) // object
  var entry = a.entries();
  for(var i=0; i<a.length;i++){
-   console.log(entry.next().value);
+   // returns the value and index of an array
+   console.log(entry.next().value); //[ 0, 'a' ] [ 1, 'b' ] [ 2, 'c' ]
+
  }
+
+
+// using for...of loop
+
+var fruits = ['apple','mango','orange'];
+var fruit = fruits.entries();
+for(var ft of fruit){
+  console.log(ft);
+}
