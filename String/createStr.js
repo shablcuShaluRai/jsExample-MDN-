f90d2322da02760779d325b687873e5f8bc9ee5c
shalurai <shaluraics22@gmail.com>
2017-08-15 15:57:09 +0900

    create string

---
+++ String/createStr.js
@@ -0,0 +1,15 @@
+// string is a collection of characters
+console.log("hello javascript learner with shalu");
+
+
+// the length property represents the length of string
+
+var fruits = "pomegranate"
+console.log(fruits.length); // returns 11
+
+var empty = " ";
+// it returns 1 because this string has a space.
+console.log(empty.length);
+
+var nothing = '';
+console.log(nothing.length); // it returns 0
